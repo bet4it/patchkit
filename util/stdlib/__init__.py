019763d1e041819aa691240330fbdc852d44d539
Bet4 <16643669+bet4it@users.noreply.github.com>
2018-04-06 13:34:40 +0800

    Add basic initialization for lib43

--- util/stdlib/__init__.py
+++ util/stdlib/__init__.py
@@ -7,4 +7,12 @@ __all__ = ['declare']
 
 def declare(linker):
     if not '_terminate' in linker:
-        pass
+        linker.declare(headers=read('stdlib/lib43/src/io.h'))
+        linker.declare(headers=read('stdlib/lib43/src/mem.h'))
+        linker.declare(headers=read('stdlib/lib43/src/string.h'))
+        linker.declare(headers=read('stdlib/lib43/src/syscall.h'))
+        linker.declare(headers=read('stdlib/lib43/src/syscalls.h'))
+        linker.autodecl(read('stdlib/lib43/src/io.c'))
+        linker.autodecl(read('stdlib/lib43/src/mem.c'))
+        linker.autodecl(read('stdlib/lib43/src/string.c'))
+        linker.autodecl(read('stdlib/lib43/src/syscalls.c'))
